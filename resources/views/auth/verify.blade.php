55c58a352a163fd6f51d3f84919d0ffe0e10e344
talentbz <82767636+talentbz@users.noreply.github.com>
2021-12-29 17:45:37 +0800

    change login page

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,155 +4,74 @@
     Verify Password
 @endsection
 
-@section('css')
-    <!-- owl.carousel css -->
-    <link rel="stylesheet" href="{{ URL::asset('/assets/libs/owl.carousel/owl.carousel.min.css') }}">
-@endsection
-
 @section('body')
 
-    <body class="auth-body-bg">
+    <body>
     @endsection
 
     @section('content')
-
-        <div>
-            <div class="container-fluid p-0">
-                <div class="row g-0">
-
-                    <div class="col-xl-9">
-                        <div class="auth-full-bg pt-lg-5 p-4">
-                            <div class="w-100">
-                                <div class="bg-overlay"></div>
-                                <div class="d-flex h-100 flex-column">
-
-                                    <div class="p-4 mt-auto">
-                                        <div class="row justify-content-center">
-                                            <div class="col-lg-7">
-                                                <div class="text-center">
-
-                                                    <h4 class="mb-3"><i
-                                                            class="bx bxs-quote-alt-left text-primary h1 align-middle me-3"></i><span
-                                                            class="text-primary">5k</span>+ Satisfied clients</h4>
-
-                                                    <div dir="ltr">
-                                                        <div class="owl-carousel owl-theme auth-review-carousel"
-                                                            id="auth-review-carousel">
-                                                            <div class="item">
-                                                                <div class="py-3">
-                                                                    <p class="font-size-16 mb-4">" Fantastic theme with a
-                                                                        ton of options. If you just want the HTML to
-                                                                        integrate with your project, then this is the
-                                                                        package. You can find the files in the 'dist'
-                                                                        folder...no need to install git and all the other
-                                                                        stuff the documentation talks about. "</p>
-
-                                                                    <div>
-                                                                        <h4 class="font-size-16 text-primary">Abs1981</h4>
-                                                                        <p class="font-size-14 mb-0">- Skote User</p>
-                                                                    </div>
-                                                                </div>
-
-                                                            </div>
-
-                                                            <div class="item">
-                                                                <div class="py-3">
-                                                                    <p class="font-size-16 mb-4">" If Every Vendor on Envato
-                                                                        are as supportive as Themesbrand, Development with
-                                                                        be a nice experience. You guys are Wonderful. Keep
-                                                                        us the good work. "</p>
-
-                                                                    <div>
-                                                                        <h4 class="font-size-16 text-primary">nezerious</h4>
-                                                                        <p class="font-size-14 mb-0">- Skote User</p>
-                                                                    </div>
-                                                                </div>
-
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- end col -->
-
-                    <div class="col-xl-3">
-                        <div class="auth-full-page-content p-md-5 p-4">
-                            <div class="w-100">
-
-                                <div class="d-flex flex-column h-100">
-                                    <div class="mb-4 mb-md-5">
-                                        <a href="index" class="d-block auth-logo">
-                                            <img src="{{ URL::asset('/assets/images/logo-dark.png') }}" alt="" height="18"
-                                                class="auth-logo-dark">
-                                            <img src="{{ URL::asset('/assets/images/logo-light.png') }}" alt="" height="18"
-                                                class="auth-logo-light">
-                                        </a>
-                                    </div>
-                                    <div class="my-auto">
-
-                                        <div>
+        <div class="account-pages my-5 pt-sm-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card overflow-hidden">
+                            <div class="bg-primary bg-soft">
+                                <div class="row">
+                                    <div class="col-7">
+                                        <div class="text-primary p-4">
                                             <h5 class="text-primary"> Verify Password</h5>
-                                            <p class="text-muted">Re-Password with Skote.</p>
-                                        </div>
-
-                                        <div class="mt-4">
-                                            <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                                                @csrf
-                                                <div class="mb-3">
-                                                    <label for="username">email</label>
-                                                    <input name="email" type="email"
-                                                        class="form-control @error('email') is-invalid @enderror"
-                                                        @if (old('email'))
-                                                    value="{{ old('email') }}" @endif id="email"
-                                                    placeholder="Enter email" autocomplete="email" autofocus>
-                                                    @error('email')
-                                                        <span class="invalid-feedback" role="alert">
-                                                            <strong>{{ $message }}</strong>
-                                                        </span>
-                                                    @enderror
-                                                </div>
-
-                                                <div class="text-end">
-                                                    <button class="btn btn-primary w-md waves-effect waves-light"
-                                                        type="submit">Reset</button>
-                                                </div>
-                                            </form>
-                                            <div class="mt-5 text-center">
-                                                <p>Remember It ? <a href="{{ url('login') }}"
-                                                        class="font-weight-medium text-primary"> Sign In here</a> </p>
-                                            </div>
+                                            <p>Verify Password with Skote.</p>
                                         </div>
                                     </div>
-
-                                    <div class="mt-4 mt-md-5 text-center">
-                                        <p class="mb-0">© <script>
-                                                document.write(new Date().getFullYear())
-
-                                            </script> Skote. Crafted with <i class="mdi mdi-heart text-danger"></i> by
-                                            Themesbrand</p>
+                                    <div class="col-5 align-self-end">
+                                        <img src="{{ URL::asset('/assets/images/profile-img.png') }}" alt="" class="img-fluid">
                                     </div>
                                 </div>
                             </div>
+                            <div class="card-body pt-0">
+                                <div>
+                                    <a href="index">
+                                        <div class="avatar-md profile-user-wid mb-4">
+                                            <span class="avatar-title rounded-circle bg-light">
+                                                <img src="{{ URL::asset('/assets/images/logo.svg') }}" alt="" class="rounded-circle" height="34">
+                                            </span>
+                                        </div>
+                                    </a>
+                                </div>
+
+                                @if (session('resent'))
+                                    <div class="alert alert-success" role="alert">
+                                        {{ __('A fresh verification link has been sent to your email address.') }}
+                                    </div>
+                                @endif
+
+                                {{ __('Before proceeding, please check your email for a verification link.') }}
+                                {{ __('If you did not receive the email') }},
+                                <div class="p-2">
+                                    <form class="form-horizontal" method="POST" action="{{ route('verification.resend') }}">
+                                        @csrf
+
+                                        <div class="text-end">
+                                            <button class="btn btn-primary w-md waves-effect waves-light"
+                                                type="submit">{{ __('click here to request another') }}</button>
+                                        </div>
+                                        
+                                    </form>
+                                </div>
+
+                            </div>
                         </div>
+                        <div class="mt-5 text-center">
+                            <p>Remember It ? <a href="{{ url('login') }}" class="fw-medium text-primary"> Sign In here</a> </p>
+                            <p>© <script>
+                                    document.write(new Date().getFullYear())
+
+                                </script> Skote. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
+                        </div>
+
                     </div>
-                    <!-- end col -->
                 </div>
-                <!-- end row -->
             </div>
-            <!-- end container-fluid -->
         </div>
 
-    @endsection
-
-    @section('script')
-        <!-- owl.carousel js -->
-        <script src="{{ URL::asset('/assets/libs/owl.carousel/owl.carousel.min.js') }}"></script>
-        <!-- auth-2-carousel init -->
-        <script src="{{ URL::asset('/assets/js/pages/auth-2-carousel.init.js') }}"></script>
     @endsection
